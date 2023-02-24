7078278b35b532830b89dc1fc0e66f9f22849dd0
Oma_goodness <oluomaihejiofor@gmail.com>
2023-02-24 18:45:26 +0300

    Done twith total

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -22,4 +22,4 @@ if __name__ == "__main__":
 
     print("Employee {} is done with tasks({}/{}):".format(employee_name,
           task_completed, total_task_done))
-    [print("\t {}".format(task.get("title"))) for task in task_completed]
+    [print("\t {}".format(task.get("title"))) for task in total_tasks]
